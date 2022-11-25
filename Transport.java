7fb96aad7a46f44a531b9d266f7344ff4da4037a
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-11-25 20:03:20 +0900

    3WSA

--- Transport.java
+++ Transport.java
@@ -27,20 +27,31 @@ public class Transport {
 
 
     public static void main(String[] args) {
+        // 객체 생성
         Taxi taxi = new Taxi();
         Bus bus = new Bus();
+        // 버스 승객 탑승 2명
         bus.inPassenger(2);
+        // 버스 주유량 - 50
         bus.setOil(-50);
+        // 버스 차고지행
         bus.setCondition("차고지행");
+        // 버스 주유량 + 10
         bus.setOil(10);
 //        bus.inPassenger(45); 최대 승객 수 초과
+        // 버스 승객 탑승 5명
         bus.inPassenger(5);
+        // 버스 승객 탑승 5명
         bus.inPassenger(5);
 //        bus.setOil(-55); 주유 필요
+        // 택시 승객 탑승 2명 서울역 2km
         taxi.inPassenger(2, "서울역", 2);
+        // 택시 주유량 - 80
         taxi.setOil(-80);
+        // 택시 요금 결제
         taxi.pay();
 //        taxi.inPassenger(5, "서울역", 2); 최대 승객 수 초과
+        // 택시 승객 탑승 3명 구로디지털단지역 12km
         taxi.inPassenger(3, "구로디지털단지역", 12);
 //        taxi.setOil(-20); 주요 필요
     }
